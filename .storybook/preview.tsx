2a393310e90abf8a8d98237d48ef3be43255b3a5
Nguyen Quyen <tomcon981@gmail.com>
2023-07-05 00:43:26 +0700

    setup

---
+++ .storybook/preview.tsx
@@ -0,0 +1,20 @@
+import type { Preview } from "@storybook/react";
+
+// Load Roboto fonts
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@mui/icons-material";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      expanded: true,
+      hideNoControlsWarning: true,
+    },
+  },
+};
+
+export default preview;
